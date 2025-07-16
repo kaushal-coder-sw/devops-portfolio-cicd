12a5ae4fe75984ed06ff2edbdfa3a32c61d7fd78
Kaushal <kaushaljujgar1@gmail.com>
2025-07-16 21:26:18 +0530

    [ DevOps-Portfolio-Cicd - Changed IP in helathcheck script -01]

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-APP_URL="http://13.232.240.157"
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL)
+APP_URL="http://3.7.248.20"
+
+MAX_RETRIES=20
+RETRY_DELAY=3
+COUNT=0
+STATUS=000
+
+while [ "$STATUS" -ne 200 ] && [ $COUNT -lt $MAX_RETRIES ]; do
+  echo "⌛ Waiting for app to become healthy... (attempt $((COUNT+1)))"
+  sleep $RETRY_DELAY
+  STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL" || echo "000")
+  COUNT=$((COUNT+1))
+done
 
 if [ "$STATUS" -ne 200 ]; then
   echo "❌ Health check failed! Status: $STATUS"
